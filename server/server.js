fe18109c98278099941d19a3949b4bc9c9c368fe
philipduan <philip_du_an@hotmail.com>
2018-02-06 15:18:34 -0500

    Fix typo to public folder

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 //Make express look in the public directory for assets
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/server/public'));
 
 //lets make a simple get request to get some sort of response from our server
 app.get('/', (req, res) => {
